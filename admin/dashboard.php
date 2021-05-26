bc7d3554cfa0480f11e9151f8ba904c8d0798599
SodoTeo <sodolescuteodor@gmail.com>
2021-05-27 01:41:37 +0300

    feat: image form to admin panel

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -10,6 +10,65 @@
     {
         $user_id = $_SESSION["u_id"];
     }
+ //---------------------------IMG--------------------------------//
+
+ error_reporting( ~E_NOTICE ); // avoid notice
+
+
+ if(isset($_POST['submit']))
+ {
+ 
+	 
+	 
+	 $imgFile = $_FILES['user_image']['name'];
+	 $tmp_dir = $_FILES['user_image']['tmp_name'];
+	 $imgSize = $_FILES['user_image']['size'];
+	 
+	 if(empty($imgFile)){
+		 $errMSG = "Please Select Image File.";
+	 }
+	 else{
+		 $upload_dir = '../images/'; // upload directory
+
+		 $imgExt = strtolower(pathinfo($imgFile,PATHINFO_EXTENSION)); // get image extension
+		 
+		 // valid image extensions
+		 $valid_extensions = array('jpg'); // valid extensions
+		 
+		 // rename uploading image
+		 $userpic = rand(1000,1000000).".".$imgExt;
+		 
+		 // allow valid image file formats
+		 if(in_array($imgExt, $valid_extensions)){   
+			 // Check file size '300kb'
+			 if($imgSize < 350000) {
+				 move_uploaded_file($tmp_dir,$upload_dir.$userpic);
+			 }
+			 else{
+				 $errMSG = "Sorry, your file is too large. Check file size to be 300KB.";
+			 }
+		 }
+		 else{
+			 $errMSG = "Sorry, only JPG files are allowed.";  
+		 }
+	 }
+	 
+	 // if no error occured, continue ....
+	 if(!isset($errMSG))
+	 {
+		 $stmt = $connect->prepare('INSERT INTO images(img_id,u_foto) VALUES( :uids, :upic)');
+		 $stmt->bindParam(':uids',$user_id);
+		 $stmt->bindParam(':upic',$userpic);
+		 if($stmt->execute())
+		 {
+			 $successMSG = "new record succesfully inserted ...";
+			 header("refresh:2;"); // redirects image view page after 2 seconds.
+		 }else
+		 {
+			 $errMSG = "error while inserting....";
+		 }
+	 }
+ }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,7 +110,7 @@
                             <div class="form-header">
                                 <h1>Menu</h1>
                             </div>
-                            <form action="" method="post">
+                            <form action="" enctype="multipart/form-data" method="post">
 							<?php
                                     $result = $connect->query("SELECT * FROM hotels WHERE h_id LIKE $user_id ");
                                     if($result)
@@ -83,9 +142,82 @@
 									<button type="button" onclick="location.href='update_info.php'" style="background-color: #8ed68e;" class="submit-btn">Update your hotel</button>
                                 </div>
 								<br><br>
-								<div class="form-btn">
-									<p style="color: #E91E63;">Here wil be image gallery+form ....</p>
+								<div class="row">
+									<form action="" method="post" enctype="multipart/form-data" name="hotels">  
+										<div class="col-md-6">
+											<div class="form-group">
+												<input name="user_image" class="form-control" id="upload-photo" style="  opacity: 0; position:absolute; z-index: -1; " type="file" accept="image/*">
+												<label class="submit-btn" style="color:#8ed68e; background-color: rgba(255, 255, 255, 0.2);" for="upload-photo">Browse...</label>
+												<span class="form-label">Profile Img.</span>
+
+											</div>
+										</div>
+										<div class="col-md-6">
+											<div class="form-group">
+												<input name="submit" style="background-color: #8ed68e;" class="submit-btn" type="submit" ">
+												<span class="form-label">Update image</span>
+											</div>
+										</div>
+									</form>
                                 </div>
+					
+
+					
+								<!--/ ---------------------------IMG-------------------------------- /-->
+								<?php
+                require_once 'db/config.php';
+                if(isset($_GET['delete_id']))
+                {
+                    // select image from db to delete
+                    $stmt_select = $connect->prepare('SELECT u_foto FROM images WHERE u_foto =:uid');
+                    $stmt_select->execute(array(':uid'=>$_GET['delete_id']));
+                    $imgRow=$stmt_select->fetch(PDO::FETCH_ASSOC);
+                    unlink("../images/".$imgRow['u_foto']);
+                    
+                    // it will delete an actual record from db
+                    $stmt_delete = $connect->prepare('DELETE FROM images WHERE u_foto =:uid');
+                    $stmt_delete->bindParam(':uid',$_GET['delete_id']);
+                    $stmt_delete->execute();
+      
+                }
+                ?>
+                    <?php
+                    
+                    $stmt = $connect->prepare("SELECT u_foto FROM images WHERE img_id LIKE $user_id ");
+                    $stmt->execute();
+                    
+                    if($stmt->rowCount() > 0)
+                    {
+                        while($row=$stmt->fetch(PDO::FETCH_ASSOC))
+                        {
+                            extract($row);
+                            ?>
+
+                            <div class='row'>
+                             
+                                    <div class='col-md-6 well'>
+                                        <img src="../images/<?php echo $row['u_foto']; ?>" class="img-rounded" width="30%"  /> 
+                                        <a class="btn" style="color: #ffbb2b;" href="?delete_id=<?php echo $row['u_foto']; ?>" title="click for delete" onclick="return confirm('sure to delete ?')"><span class="glyphicon glyphicon-remove-circle"></span> Delete</a>
+                                    </div>    
+                            </div>
+							<br>   
+
+                            <?php
+                        }
+                    }else
+                    {
+                        ?>
+                            <div class='row'>
+                            
+                                    <div class='col-md-6 well'>
+                                        <div class="alert alert-warning">
+                                            <span class="glyphicon glyphicon-info-sign"></span> &nbsp; No Image Found ...
+                                        </div>
+                                    </div>    
+                            </div> 
+                        <?php
+                    }
+                    ?>
                             </form>
                         </div>
                     </div>
